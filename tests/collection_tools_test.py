323b1bfe9f3b92cae7e213a38a56284760c9b45f
Francisco Santiago <beat@beat.local>
2017-08-15 14:34:25 +0200

    added tests

---
+++ tests/collection_tools_test.py
@@ -0,0 +1,58 @@
+import os
+import unittest
+import json
+
+import pandas as pd
+import pandas.util.testing as pdt
+
+from utils import collection_tools
+
+class TestBase(unittest.TestCase):
+    def test_build_hierarchy(self):
+        artists = [
+            {
+                "genre": "blues", 
+                "decade": "50", 
+                "name": "muddy waters"
+            },
+            {
+                "genre": "hip hop", 
+                "decade": "90", 
+                "name": "kriss kross"
+            },
+            {
+                "genre": "blues", 
+                "decade": "90", 
+                "name": "buddy guy"
+            },
+            {
+                "genre": "rock", 
+                "decade": "80", 
+                "name": "rolling stones"
+            },
+            {
+                "genre": "rock", 
+                "decade": "90", 
+                "name": "rolling stones"
+            }
+        ]
+
+        with self.assertRaises(collection_tools.NoFieldsError):
+            collection_tools.build_hierarchy(artists, fields=[])
+
+        with self.assertRaises(collection_tools.FormatError):
+            collection_tools.build_hierarchy(artists, fields='sala')
+
+        decades = collection_tools.build_hierarchy(artists, fields=['decade'])
+        self.assertEquals(len(decades), 3)
+        for node in decades:
+            self.assertTrue(node['title'] in ('50', '80', '90'))
+
+        artist_nodes = collection_tools.build_hierarchy(artists, fields=['name', 'decade'])
+        self.assertEquals(len(artist_nodes), 4)
+        for node in artist_nodes:
+            # Check that all names are in the first level
+            self.assertTrue(node['title'] in ('kriss kross', 'buddy guy', 'muddy waters', 'rolling stones'))
+            for sub_node in node['nodes']:
+                # Check that decades are in the second level
+                self.assertTrue(sub_node['title'] in ('50', '80', '90'))
--- utils/collection_tools.py
+++ utils/collection_tools.py
@@ -1,14 +1,13 @@
-def build_hierarchy(elements, fields):
-    """Takes a list of dicts and groups them in an hierarchical way
-    respecting the order provied by "fields".
-    First field is used to group the first layer.
-    
-    :param elements: list of JSON objects
-    :param fields: list of fields to use as grouping keys. Order matters
-    :return: a JSON list with the format [
-        {title: "something1", nodes:[{title: "something2", nodes:[...]}]}] 
-    """
-    
+import itertools
+
+
+class NoFieldsError(Exception):
+    pass
+
+class FormatError(Exception):
+    pass
+
+def recursive_hierarchy(elements, fields):
     if not fields:
         return elements
 
@@ -23,9 +22,26 @@ def build_hierarchy(elements, fields):
     for k, g in itertools.groupby(ordered_elements, lambda el: el[field]):
         node = {
             'title': k,
-            'nodes': build_hierarchy(list(g), fields)
+            'nodes': recursive_hierarchy(list(g), fields)
         }
 
         nodes.append(node)
 
     return nodes
+
+def build_hierarchy(elements, fields):
+    """Takes a list of dicts and groups them in an hierarchical way
+    respecting the order provied by "fields".
+    First field is used to group the first layer.
+    
+    :param elements: list of JSON objects
+    :param fields: list of fields to use as grouping keys. Order matters
+    :return: a JSON list with the format [
+        {title: "something1", nodes:[{title: "something2", nodes:[...]}]}] 
+    """
+    if not fields:
+        raise NoFieldsError('Please provide an iterable of key strings')
+    elif not isinstance(fields, list):
+        raise FormatError("The 'fields' parameter must be of type <list>")
+
+    return recursive_hierarchy(elements, fields)
